61a1aa92257e8d34bfb5141156c3478f817d206d
Vincent To <vincent_to@139.com>
2024-04-24 04:20:10 +0800

    Patch fixes #3

--- src/components/PoolEnquiryForm.vue
+++ src/components/PoolEnquiryForm.vue
@@ -14,7 +14,7 @@
     <div class="column is-3"></div>
     <div class="column is-6">
       <!-- Form Content -->
-      <form name="enquiry-form" method="POST" data-netlify="true">
+      <form name="contact" netlify>
         <div class="field">
           <label class="label self_p1 is-medium">Name</label>
           <div class="control">
